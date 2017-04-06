a618985b47782928c80480eff01916f0e0cfdb85
AaronBruce <aaron.bruce@comcast.net>
2017-04-06 14:12:12 -0700

    Added functions to questions

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Week-1-d21
 Homework repo for 201d21
 author Adan Rabadan,
+
+
+Day-004 Lab-004 Updated
+Driver: Aaron
+Navigator: Adan
+
+Added functions to all seven existing questions and ran each one separately to confirm that they were working.
+
+Question 7 poses complications as it is not working properly due to some other issue. Still working on it.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 var userScore = 0;
 
+function storiesQuestion() {
 
 var question1 = prompt('Do you like scary stories?');
 if (question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y') {
@@ -11,7 +12,10 @@ if (question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y') {
 } else {
   alert('No need to get scared and start typing out control');
 }
+}
+storiesQuestion();
 
+function rainQuestion() {
 var question2 = prompt('Is it raining outside?');
 if (question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y') {
   alert('That Seattle for you.');
@@ -21,7 +25,10 @@ if (question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y') {
 } else {
   alert('Simple yes or no question.');
 }
+}
+rainQuestion();
 
+function theBatman() {
 var question3 = prompt('Do you like Batman?');
 if (question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y') {
   alert('Batman is awesome!.');
@@ -31,7 +38,10 @@ if (question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y') {
 } else {
   alert('Simple yes or no question.');
 }
+}
+theBatman();
 
+function tacos() {
 var question4 = prompt('Do you like tacos?');
 if (question4.toLowerCase() === 'yes' || question4.toLowerCase() === 'y') {
   alert('Tacos are the best');
@@ -41,8 +51,11 @@ if (question4.toLowerCase() === 'yes' || question4.toLowerCase() === 'y') {
 } else {
   alert('Simple yes or no question.');
 }
+}
+tacos();
 
-var question5 = prompt('Do you play video game?');
+function videoGames() {
+var question5 = prompt('Do you play video games?');
 if (question5.toLowerCase() === 'yes' || question5.toLowerCase() === 'y') {
   alert('Video games are my favorite pass time');
   userScore++;
@@ -51,12 +64,14 @@ if (question5.toLowerCase() === 'yes' || question5.toLowerCase() === 'y') {
 } else {
   alert('Simple yes or no question.');
 }
+}
+videoGames();
 
 
 //question 6
 var correctGuess = false;
 
-
+function kids() {
 for (var x = 4; x > 0 && !correctGuess; x--) {
   var question6 = prompt('Can you guess how many kids I have?');
   var parsed = parseInt(question6);
@@ -72,13 +87,17 @@ for (var x = 4; x > 0 && !correctGuess; x--) {
     alert('That\'s not an answer!');
   }
 }
+}
+kids();
 
 
 
 //Question 7
+
 var newArray = ['England','Mexico','Germany','Korea', 'Turkey','Russia'];
 var countryGuess = false;
 
+function countriesLived() {
 for (var i = 0; i < newArray.length && !countryGuess; i++){
   var question7 = prompt('Can you guess what country I\'ve travel to while in the military?').toLowerCase();
   if (question7.toLowerCase() === newArray[i].toLowerCase()) {
@@ -90,8 +109,10 @@ for (var i = 0; i < newArray.length && !countryGuess; i++){
     alert('Hint: One of the countries is connected to the USA');
   }
 }
-
-
-
-
-alert('Your score is ' + userScore + ' out of 7');
+}
+countriesLived();
+//
+//
+//
+//
+// alert('Your score is ' + userScore + ' out of 7');
